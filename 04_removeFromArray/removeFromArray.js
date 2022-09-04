eb91d2df8247a91fe45f80426611323809c3df88
jleesys949 <jlee.post24@gmail.com>
2022-09-03 21:38:14 -0700

    Ignores non present values

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -2,6 +2,9 @@ const removeFromArray = function (array, element) {
     let arrayArgs = [...arguments];
     for (let i = 1; i < arguments.length; i++) {
         let index = array.indexOf(arrayArgs[i]);
+        if (index == -1) {
+            continue;
+        }
         array.splice(index, 1);
     }
     return array;
