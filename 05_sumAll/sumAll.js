9bc997d1f6548d8c2108963ad7996393369b1934
jleesys949 <jlee.post24@gmail.com>
2022-09-03 21:56:05 -0700

    Sums numbers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+    let sum = 0;
+    if ( a > b ) {
+        let c = a;
+        a = b; b = c;
+    }
+    for (let i = a; i <= b; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
