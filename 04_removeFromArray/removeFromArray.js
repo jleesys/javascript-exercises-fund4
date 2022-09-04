093e3b96fa0114c2649a16e456d8ef5e6cc89515
jleesys949 <jlee.post24@gmail.com>
2022-09-03 21:29:29 -0700

    Clean

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,5 @@
 const removeFromArray = function(array,element) {
     let index = array.indexOf(element);
-    // let newArray = array.splice(index,1);
     array.splice(index,1);
     return array;
 };
