1f352420a6f20d15ce542fbf4503b7d219e38f54
jleesys949 <jlee.post24@gmail.com>
2022-09-03 21:35:42 -0700

    Can remove multiple values

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,9 @@
-const removeFromArray = function(array,element) {
-    let index = array.indexOf(element);
-    array.splice(index,1);
+const removeFromArray = function (array, element) {
+    let arrayArgs = [...arguments];
+    for (let i = 1; i < arguments.length; i++) {
+        let index = array.indexOf(arrayArgs[i]);
+        array.splice(index, 1);
+    }
     return array;
 };
 
