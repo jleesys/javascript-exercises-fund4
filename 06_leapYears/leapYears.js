0ae46a5ca49215bab80db6ed3db41987c0060fa3
jleesys949 <jlee.post24@gmail.com>
2022-09-03 22:23:30 -0700

    Complete function

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,18 @@
 const leapYears = function() {
+    const arrayArgs = Array.from(arguments);
+    let input = arrayArgs[0];
+
+    // if not divisible by 4, it is NOT a leap year
+    if ( input % 4 != 0 ) return false;
+    // if divisible by 400 then it is a leap year
+    if ( input % 400 == 0 ) return true;
+    // if it is divisible by 100 then it is not a leap year
+    if ( input % 100 == 0 ) return false;
+    if ( input % 4 == 0) return true;
+
+
+    return false;
+
 
 };
 
