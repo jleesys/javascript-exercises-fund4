b0ade81014fa293b5fdcaf54f1ccdb3f03576194
jleesys949 <jlee.post24@gmail.com>
2022-09-21 18:56:46 -0700

    Complete exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,21 @@
-const palindromes = function () {
+const palindromes = function (input) {
+    input = input.trim().toLowerCase();
+    let inputAsArray = Array.from(input);
+    let newArray = [];
+    for (let i = 0; i < inputAsArray.length; i++) {
+        if (isLetter(inputAsArray[i])) newArray.push(inputAsArray[i]);
+    }
+
+    return newArray.join() == newArray.reverse().join();
 
 };
+
+function isLetter(string) {
+    if ((string.length == 1) && (string.match(/[a-z]/i))) {
+        return true;
+    }
+    return false;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
