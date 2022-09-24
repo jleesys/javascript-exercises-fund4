845fa992d093ba01fbc7c23a24c25bd51d00916e
jleesys949 <jlee.post24@gmail.com>
2022-09-24 15:52:05 -0700

    Complete exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let currentDate = new Date();
+    let currentYear = currentDate.getFullYear();
+    people.map(person => {
+        if (person.yearOfDeath == undefined) person.yearOfDeath = currentYear; 
+    });
+    people.sort((a,b) =>((a.yearOfDeath - a.yearOfBirth) > (b.yearOfDeath - b.yearOfBirth) ? -1 : 1));
+    return people[0];
 };
 
 // Do not edit below this line
