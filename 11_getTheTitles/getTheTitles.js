33b5a15b9581cbcffeb2f3ee12332ebcb4031fb3
jleesys949 <jlee.post24@gmail.com>
2022-09-24 15:01:53 -0700

    Complete function

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    const arrayTitles = books.map(book => book.title);
+    return arrayTitles;
 };
 
 // Do not edit below this line
