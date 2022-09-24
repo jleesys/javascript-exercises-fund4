a6dd69c1b4c32e0abb4bd0bcc4c13b535ae7b728
jleesys949 <jlee.post24@gmail.com>
2022-09-24 14:39:55 -0700

    Fix comment

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -20,7 +20,7 @@ const fibonacci = function(input) {
 fibonacci sequence ex
 > A series of numbers in which each number ( Fibonacci number ) 
 is the sum of the two preceding numbers. The simplest is the series 1, 1, 2, 3, 5, 8, etc.
-/*
+*/
 
 // Do not edit below this line
 module.exports = fibonacci;
